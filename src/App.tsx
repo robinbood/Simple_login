3ecb300d287ca21bff57065bf9168ec2fa26f790
robinbood <mercedesfarts@gmail.com>
2025-05-17 08:45:41 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,21 @@ const App = () => {
   const [userId, setUserId] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [yay, setYay] = useState<string>("");
+  const [userErr,setUserErr] = useState<boolean>(false)
+  const [passwordErr,setPasswordErr] = useState<boolean>(false)
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (userId === "shit" && password === "fuck"){
       setYay("yay you did it  right");
+    }
+    if (userId !== "shit") {
+      setUserErr(true)
+      setUserId("")
+    }
+    if(password !== "fuck") {
+      setPasswordErr(true)
+      setPassword("")
     }
     setPassword("");
     setUserId("");
@@ -21,14 +31,20 @@ const App = () => {
   return (
     <>
       <div>
-        <form onSubmit={onSubmit}>
-          User:<input
+        <form onSubmit={onSubmit} >
+          <input
             type="text"
+            id="userid"
+            required
+            style={{borderColor:userErr ? "red" : "initial"}}
             onChange={({ target }) => setUserId(target.value)}
           />
           <br />
-          Password:<input
+          <input
             type="password"
+            id="password"
+            required
+            style={{borderColor:passwordErr ? "red" : "initial"}}
             onChange={({ target }) => setPassword(target.value)}
           />
           <br />
